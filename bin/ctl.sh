b48f6f9dbcb8208de23520acca61976dae248de3
clemens-zauchner <cst.zauchner@gmail.com>
2022-02-16 12:14:08 +0100

    Initial commit

---
+++ bin/ctl.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e 
+
+export BASE_PATH="$(realpath $(dirname $0)/..)"
+
+DJANGO_PATH="${BASE_PATH}/django_wedding_website"
+export CONFIG_PATH="${BASE_PATH}/config"
+export DJANGO_CONF_FILE="${CONFIG_PATH}/django.conf"
+export DB_CONF_FILE="${CONFIG_PATH}/db.conf"
+
+cd "${BASE_PATH}"
+
+if [ $1 = "build" ]
+then
+    WEBSITE_PATH="${BASE_PATH}/docker/website/files"
+    cp "${DJANGO_PATH}/manage.py" "${WEBSITE_PATH}/." 
+    cp -r "${DJANGO_PATH}/wedding/" "${WEBSITE_PATH}/." 
+    cp -r "${DJANGO_PATH}/wedding_website/" "${WEBSITE_PATH}/." 
+    export DOCKER_BUILDKIT=1
+fi
+export COMPOSE_PROJECT_NAME="wedding"
+COMPOSE_PATH="${BASE_PATH}/docker-compose"
+COMPOSE_FILE="${COMPOSE_PATH}/docker-compose.yml"
+
+
+docker-compose -f "${COMPOSE_FILE}" "$@"
+
+if [ $1 = "build" ]
+then
+    rm "${WEBSITE_PATH}/manage.py"
+    rm -rf "${WEBSITE_PATH}/wedding"
+    rm -rf "${WEBSITE_PATH}/wedding_website"
+fi
